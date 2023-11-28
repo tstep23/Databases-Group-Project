baf93ba84937445a01cd6308567ef433bb28d330
Henry Do <hdo9@lsu.edu>
2023-11-27 20:33:41 -0600

    updated db code

--- DB_code.sql
+++ DB_code.sql
@@ -1,52 +1,27 @@
 -- Table: Schedule
 CREATE TABLE schedule (
     ID INTEGER PRIMARY KEY,
-    date INTEGER,
-    start_time TIME,
-    hours INTEGER CHECK (hours >= 1 AND hours <=12)
+    date INTEGER CHECK (length(date) = 4),
+    start_time INTEGER CHECK (length(start_time) = 4),
+    hours INTEGER CHECK (hours BETWEEN 1 AND 12)
+    w_ID INTEGER,
+    FOREIGN KEY (w_ID) REFERENCES workers(ID)
 );
 
 -- Table: Customer
 CREATE TABLE customer (
     ID INTEGER PRIMARY KEY,
-    Email VARCHAR(255) CHECK (REGEXP_LIKE(email, '^[a-zA-Z0-9._%+-]+@(gmail|yahoo|pobox)\.com$')),
-    Name VARCHAR(255),
-    points INTEGER DEFAULT 0
-);
-
--- Table: Product_Sale
-CREATE TABLE product_sale (
-    p_ID INTEGER,
-    t_ID INTEGER,
-    FOREIGN KEY (p_ID) REFERENCES products(ID),
-    FOREIGN KEY (t_ID) REFERENCES transaction(ID),
-    PRIMARY KEY (p_ID, t_ID)
-);
-
--- Table: Worker_Sale
-CREATE TABLE worker_sale (
-    w_ID INTEGER,
-    t_ID INTEGER,
-    FOREIGN KEY (w_ID) REFERENCES workers(ID),
-    FOREIGN KEY (t_ID) REFERENCES transaction(ID),
-    PRIMARY KEY (w_ID, t_ID)
-);
-
--- Table: Order_History
-CREATE TABLE order_history (
-    c_ID INTEGER,
-    t_ID INTEGER,
-    FOREIGN KEY (c_ID) REFERENCES customer(ID),
-    FOREIGN KEY (t_ID) REFERENCES transaction(ID),
-    PRIMARY KEY (c_ID, t_ID)
+    email TEXT CHECK (email LIKE '%_@%.%'),
+    name TEXT,
+    points INTEGER CHECK (points >= 0)
 );
 
 -- Table: Workers 
-CREATE TABLE Workers (
+CREATE TABLE workers (
     ID INTEGER PRIMARY KEY,
-    NAME VARCHAR(255),
-    phone_number BIGINT CHECK (LENGTH(phone_number) = 10),
-    bank_number BIGINT CHECK (LENGTH(bank_number) >= 8 AND LENGTH(bank_number) <= 12),
+    name TEXT,
+    phone_number INTEGER CHECK (length(phone_number) = 10),
+    bank_number INTEGER CHECK (length(bank_number) BETWEEN 8 AND 12),
     wage DECIMAL CHECK ((type = 'e' AND wage >= 12 AND wage <= 25) OR (type = 'm' AND wage >= 15 AND wage <= 30)),
     type CHAR CHECK (type IN ('e', 'm'))
 );
@@ -54,9 +29,9 @@ CREATE TABLE Workers (
 -- Table: Products 
 CREATE TABLE products (
     ID INTEGER PRIMARY KEY,
-    Name VARCHAR(255),
+    Name TEXT,
     count INTEGER CHECK (count >= 0 AND count <= 20),
-    Price DECIMAL
+    price DECIMAL
 );
 
 -- Table: Transaction 
@@ -64,14 +39,11 @@ CREATE TABLE transaction (
     ID INTEGER PRIMARY KEY,
     count INTEGER,
     money DECIMAL,
-    date INTEGER
-);
-
--- Table: Shifts 
-CREATE TABLE shifts (
+    date INTEGER CHECK (length(date) = 8),
+    p_ID INTEGER,
     w_ID INTEGER,
-    s_ID INTEGER,
+    c_ID INTEGER,
+    FOREIGN KEY (p_ID) REFERENCES products(ID),
     FOREIGN KEY (w_ID) REFERENCES workers(ID),
-    FOREIGN KEY (s_ID) REFERENCES schedule(ID),
-    PRIMARY KEY (w_ID, s_ID)
+    FOREIGN KEY (c_ID) REFERENCES customer(ID)
 );
