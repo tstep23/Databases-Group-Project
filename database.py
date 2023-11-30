7facdc7c408ad4480af09a72a3f455a6094581a1
dutonix <dutonix@gmail.com>
2023-11-30 04:47:11 -0600

    starter ui

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import dbmain
 
 def connect_to_database(database_project):
     return sqlite3.connect(database_project)
@@ -15,3 +16,126 @@ def fetch_data(conn, query):
 
 def close_connection(conn):
     conn.close()
+    
+def worker_menu(conn):
+    while True:
+        print("Enter employee type: ")
+        print("1. Employee")
+        print("2. Manager")
+        print("3. Exit")
+        worker_choice = input("Enter your choice: ")
+        
+        if worker_choice == "1":
+            print("Employee Menu:")
+            print("1. View Schedule")
+            print("2. View Products")
+            print("3. View Wage")
+            print("3. Exit")
+            employee_choice = input("Enter your choice: ")
+            
+            if employee_choice == "1":
+                # Call function to display schedule
+                dbmain.get_schedule_input()
+            elif employee_choice == "2":
+                # Call function to display products
+                dbmain.get_products_input()
+            elif employee_choice == "3":
+                dbmain.get_wage_input() # TODO: Implement this function
+            elif employee_choice == "4":
+                break
+            else:
+                print("Invalid input, please try again.")
+            
+        elif worker_choice == "2":
+            print("Manager Menu:")
+            print("1. View Schedule")
+            print("2. View Products")
+            print("3. View Wage")
+            print("3. Exit")
+            employee_choice = input("Enter your choice: ")
+            
+            if employee_choice == "1":
+                # Call function to display schedule
+                dbmain.get_schedule_input()
+            elif employee_choice == "2":
+                # Call function to display products
+                dbmain.get_products_input()
+            elif employee_choice == "3":
+                dbmain.get_wage_input() # TODO: Implement this function
+            else:
+                print("Invalid input, please try again.")
+        elif worker_choice == "3":
+            break
+        else:
+            print("Invalid input, please try again.")
+        
+            
+def customer_menu(conn):
+    while True:
+        print("Customer Menu:")
+        print("1. View Products")
+        print("2. View Transactions")
+        print("3. Exit")
+        
+        customer_choice = input("Enter your choice: ")
+        
+        if customer_choice == "1":
+            # Call function to display products
+            dbmain.get_products_input()
+        elif customer_choice == "2":
+            # Call function to display schedule
+            dbmain.get_schedule_input()
+        elif customer_choice == "3":
+            break
+        else:
+            print("Invalid input, please try again.")
+
+def manager_menu(conn):
+    while True:
+        print("Administrator Menu:")
+        print("1. Manage Database")
+        print("2. Exit")
+    
+        manager_choice = input("Enter your choice: ")
+        
+        if manager_choice == "1":
+            # Call function to display database menu
+            dbmain.managedb()
+        elif manager_choice == "2":
+            break
+        else:
+            print("Invalid input, please try again.")            
+            
+            
+def main():
+    # Connect to database
+    conn = connect_to_database("database_project.db")
+    
+    while True:
+        print("Welcome to the database project!")
+        print("1. Worker")
+        print("2. Customer")
+        print("3. Administrator")
+        print("4. Exit")
+        
+        role_choice = input("Enter your choice: ")
+        
+        if role_choice == "1":
+            # Call function to display worker menu
+            worker_menu(conn)
+        elif role_choice == "2":
+            # Call function to display customer menu
+            customer_menu(conn)
+        elif role_choice == "3":
+            # Call function to display manager menu
+            manager_menu(conn)
+        elif role_choice == "4":
+            break
+        else:
+            print("Invalid input, please try again.")
+        
+    # Close connection to database
+    close_connection(conn)
+    
+if __name__ == "__main__":
+    main()
--- dbmain.py
+++ dbmain.py
@@ -38,85 +38,86 @@ def get_transaction_input():
     return {"ID": tid, "count": tcount, "money": money, "date": tdate}
 
 # Connect to the database
-conn = database.connect_to_database('database_project.db')
+def managedb():
+    conn = database.connect_to_database('database_project.db')
 
-while True:
-    # Get user input for the table and operation
-    table = input("Enter table name (schedule, customer, workers, products, transaction, CLOSE): ").lower()
+    while True:
+        # Get user input for the table and operation
+        table = input("Enter table name (schedule, customer, workers, products, transaction, CLOSE): ").lower()
 
-    if table == "close":
-        print("Closing")
-        break  # Exit the loop and close the program
+        if table == "close":
+            print("Closing")
+            break  # Exit the loop and close the program
 
-    if table not in ["schedule", "customer", "workers", "products", "transaction"]:
-        print("Invalid table name. Enter valid table. ")
-        continue
+        if table not in ["schedule", "customer", "workers", "products", "transaction"]:
+            print("Invalid table name. Enter valid table. ")
+            continue
 
-    # Get user input for the operation
-    operation = input("Choose operation (ADD, DELETE, VIEW, UPDATE): ").upper()
+        # Get user input for the operation
+        operation = input("Choose operation (ADD, DELETE, VIEW, UPDATE): ").upper()
 
-    # Perform the selected operation
-    if operation == "ADD":
-        print(f"Enter values for the new entry in the '{table}' table:")
-        if table == "schedule":
-            data = get_schedule_input()
-        elif table == "customer":
-            data = get_customer_input()
-        elif table == "workers":
-            data = get_workers_input()
-        elif table == "products":
-            data = get_products_input()
-        elif table == "transaction":
-            data = get_transaction_input()
+        # Perform the selected operation
+        if operation == "ADD":
+            print(f"Enter values for the new entry in the '{table}' table:")
+            if table == "schedule":
+                data = get_schedule_input()
+            elif table == "customer":
+                data = get_customer_input()
+            elif table == "workers":
+                data = get_workers_input()
+            elif table == "products":
+                data = get_products_input()
+            elif table == "transaction":
+                data = get_transaction_input()
 
-        # Construct the query dynamically using placeholders
-        placeholders = ', '.join('?' for _ in data)
-        columns = ', '.join(data.keys())
-        query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders});"
+            # Construct the query dynamically using placeholders
+            placeholders = ', '.join('?' for _ in data)
+            columns = ', '.join(data.keys())
+            query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders});"
 
-        # Execute the query with the values
-        cursor = conn.cursor()
-        cursor.execute(query, tuple(data.values()))
-        conn.commit()
-        print("Data added successfully.")
-    elif operation == "DELETE":
-        entry_id = input(f"Enter ID of the entry to delete from the '{table}' table: ")
-        query = f"DELETE FROM {table} WHERE ID = ?;"
-        cursor = conn.cursor()
-        cursor.execute(query, (entry_id,))
-        conn.commit()
-        print("Entry deleted successfully.")
-    elif operation == "VIEW":
-        print(f"All entries in the '{table}' table:")
-        query = f"SELECT * FROM {table};"
-        rows = cursor.execute(query)
-        for row in rows:
-            print(row)
-    elif operation == "UPDATE":
-        entry_id = input(f"Enter ID of the entry to update in the '{table}' table: ")
-        print(f"Enter the updated details for the '{table}' table:")
-        if table == "schedule":
-            data = get_schedule_input()
-        elif table == "customer":
-            data = get_customer_input()
-        elif table == "workers":
-            data = get_workers_input()
-        elif table == "products":
-            data = get_products_input()
-        elif table == "transaction":
-            data = get_transaction_input()
+            # Execute the query with the values
+            cursor = conn.cursor()
+            cursor.execute(query, tuple(data.values()))
+            conn.commit()
+            print("Data added successfully.")
+        elif operation == "DELETE":
+            entry_id = input(f"Enter ID of the entry to delete from the '{table}' table: ")
+            query = f"DELETE FROM {table} WHERE ID = ?;"
+            cursor = conn.cursor()
+            cursor.execute(query, (entry_id,))
+            conn.commit()
+            print("Entry deleted successfully.")
+        elif operation == "VIEW":
+            print(f"All entries in the '{table}' table:")
+            query = f"SELECT * FROM {table};"
+            rows = cursor.execute(query)
+            for row in rows:
+                print(row)
+        elif operation == "UPDATE":
+            entry_id = input(f"Enter ID of the entry to update in the '{table}' table: ")
+            print(f"Enter the updated details for the '{table}' table:")
+            if table == "schedule":
+                data = get_schedule_input()
+            elif table == "customer":
+                data = get_customer_input()
+            elif table == "workers":
+                data = get_workers_input()
+            elif table == "products":
+                data = get_products_input()
+            elif table == "transaction":
+                data = get_transaction_input()
 
-        # Construct the SET clause dynamically using placeholders
-        set_clause = ', '.join(f"{key} = ?" for key in data.keys())
-        query = f"UPDATE {table} SET {set_clause} WHERE ID = ?;"
+            # Construct the SET clause dynamically using placeholders
+            set_clause = ', '.join(f"{key} = ?" for key in data.keys())
+            query = f"UPDATE {table} SET {set_clause} WHERE ID = ?;"
 
-        # Execute the query with the values
-        cursor = conn.cursor()
-        cursor.execute(query, tuple(data.values()) + (entry_id,))
-        conn.commit()
-        print("Entry updated successfully.")
-    else:
-        print("Invalid operation. Please enter ADD, DELETE, VIEW, UPDATE, or CLOSE.")
+            # Execute the query with the values
+            cursor = conn.cursor()
+            cursor.execute(query, tuple(data.values()) + (entry_id,))
+            conn.commit()
+            print("Entry updated successfully.")
+        else:
+            print("Invalid operation. Please enter ADD, DELETE, VIEW, UPDATE, or CLOSE.")
 
-# Close the connection when done
-database.close_connection(conn)
+    # Close the connection when done
+    database.close_connection(conn)
